4e70b2edd2f5c539572e4f4e16aa449cf668c64e
Santiago Martinez <santiagomartinezlt@gmail.com>
2023-06-15 12:41:10 +0200

    UTD

--- template-parts/menus/navigation.php
+++ template-parts/menus/navigation.php
@@ -13,19 +13,19 @@
 <section class="navbar">
     <nav class="nav--menu" role="navigation">
         <ul>
-            <li class="btnSubmenu"><?php _e("Bâtiments", "imaneo"); ?></li>
-            <li class="btnSubmenu" data-submenu=".submenu1"><span>Thèmes</span> <img class="cross1" src="dist/assets/icons/icon-nav.svg" alt=""></li>
-            <li class="btnSubmenu" data-submenu=".submenu2"><span>Imaginaires croisés</span> <img class="cross2" src="dist/assets/icons/icon-nav.svg" alt=""></li>
-            <li class="btnSubmenu" data-submenu=".submenu3"><span>Ressources</span> <img class="cross3" src="dist/assets/icons/icon-nav.svg" alt=""></li>
+            <li class="btnSubmenu"><?php _e("Projects", "imaneo"); ?></li>
+            <li class="btnSubmenu" data-submenu=".submenu1"><span><?php _e('Themes', 'imaneo'); ?></span> <img class="cross1" src="<?php bloginfo('template_url') ?>/dist/assets/icons/icon-nav.svg" alt=""></li>
+            <li class="btnSubmenu" data-submenu=".submenu2"><span><?php _e('Crossed Imagineries', 'imaneo'); ?></span> <img class="cross2" src="<?php bloginfo('template_url') ?>/dist/assets/icons/icon-nav.svg" alt=""></li>
+            <li class="btnSubmenu" data-submenu=".submenu3"><span><?php _e('Resources', 'imaneo'); ?></span> <img class="cross3" src="<?php bloginfo('template_url') ?>/dist/assets/icons/icon-nav.svg" alt=""></li>
         </ul>
     </nav>
 
     <nav class="nav--submenu" role="navigation">
         <ul class="submenu submenu1">
             <li><a href="<?php echo get_the_permalink(24); ?>">Néo-mauresque au Maghreb</a></li>
-            <li><a href="">Nationalisme & impérialisme</a></li>
-            <li><a href="">Un style pour les synagogues</a></li>
-            <li><a href="">Diffusion</a></li>
+            <li><a href="<?php echo get_the_permalink(158); ?>">Nationalisme & impérialisme</a></li>
+            <li><a href="<?php echo get_the_permalink(183); ?>">Un style pour les synagogues</a></li>
+            <li><a href="<?php echo get_the_permalink(184); ?>">Diffusion</a></li>
         </ul>
         <ul class="submenu submenu2">
             <li><a href="">Regards artistiques</a></li>
