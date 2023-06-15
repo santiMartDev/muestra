1d5c08d12b886521d5ab053fce1a8eb728a23674
degranofino <58522315+degranofino@users.noreply.github.com>
2023-06-15 12:30:29 +0200

    speakers sidebar debug

--- template-parts/partials/speaker/speaker-list.php
+++ template-parts/partials/speaker/speaker-list.php
@@ -59,8 +59,8 @@ $speakers = new WP_Query( $args );
             }
 
             // config extra education menu - title and buttons
-            // $sidebar_speaker_extra = array( array('title' => __('Related topcis', 'imaneo'), 'link' => '', 'blank' => false));
-            // $sidebar_speaker_extra = array_merge($sidebar_speaker_extra, $sidebar_speaker_themes);
+            $sidebar_speaker_extra = array( array('title' => __('Related topcis', 'imaneo'), 'link' => '', 'blank' => false));
+            $sidebar_speaker_extra = array_merge($sidebar_speaker_extra, $sidebar_speaker_themes);
 
             echo '<div>';
             // include module education
