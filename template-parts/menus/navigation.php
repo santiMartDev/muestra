0fedcdf008a7f8828566f761735307ec2c5328e0
Santiago Martinez <santiagomartinezlt@gmail.com>
2023-06-15 18:52:02 +0200

    Home

--- template-parts/menus/navigation.php
+++ template-parts/menus/navigation.php
@@ -6,7 +6,7 @@
  * @package      imaneo
  * @author       Rload
  * @since        1.0.0
-**/
+ **/
 
 ?>
 
@@ -14,9 +14,12 @@
     <nav class="nav--menu" role="navigation">
         <ul>
             <li class="btnSubmenu"><?php _e("Projects", "imaneo"); ?></li>
-            <li class="btnSubmenu" data-submenu=".submenu1"><span><?php _e('Themes', 'imaneo'); ?></span> <img class="cross1" src="<?php bloginfo('template_url') ?>/dist/assets/icons/icon-nav.svg" alt=""></li>
-            <li class="btnSubmenu" data-submenu=".submenu2"><span><?php _e('Crossed Imagineries', 'imaneo'); ?></span> <img class="cross2" src="<?php bloginfo('template_url') ?>/dist/assets/icons/icon-nav.svg" alt=""></li>
-            <li class="btnSubmenu" data-submenu=".submenu3"><span><?php _e('Resources', 'imaneo'); ?></span> <img class="cross3" src="<?php bloginfo('template_url') ?>/dist/assets/icons/icon-nav.svg" alt=""></li>
+            <li class="btnSubmenu" data-submenu=".submenu1"><span><?php _e('Themes', 'imaneo'); ?></span> <img
+                    class="cross1" src="<?php bloginfo('template_url') ?>/dist/assets/icons/icon-nav.svg" alt=""></li>
+            <li class="btnSubmenu" data-submenu=".submenu2"><span><?php _e('Crossed Imagineries', 'imaneo'); ?></span>
+                <img class="cross2" src="<?php bloginfo('template_url') ?>/dist/assets/icons/icon-nav.svg" alt=""></li>
+            <li class="btnSubmenu" data-submenu=".submenu3"><span><?php _e('Resources', 'imaneo'); ?></span> <img
+                    class="cross3" src="<?php bloginfo('template_url') ?>/dist/assets/icons/icon-nav.svg" alt=""></li>
         </ul>
     </nav>
 
@@ -28,14 +31,14 @@
             <li><a href="<?php echo get_the_permalink(198); ?>">Diffusion</a></li>
         </ul>
         <ul class="submenu submenu2">
-            <li><a href="">Regards artistiques</a></li>
-            <li><a href="">Parole de gens</a></li>
-            <li><a href="">Activité pédagogique</a></li>
+            <li><a href="<?php echo get_the_permalink(132); ?>">Regards artistiques</a></li>
+            <li><a href="<?php echo get_the_permalink(140); ?>">Parole de gens</a></li>
+            <li><a href="<?php echo get_the_permalink(142); ?>">Activité pédagogique</a></li>
         </ul>
         <ul class="submenu submenu3">
-            <li><a href="">Acteurs de la construction</a></li>
-            <li><a href="">Bibliographie</a></li>
-            <li><a href="">Intervenants</a></li>
+            <li><a href="<?php echo get_the_permalink(129); ?>">Acteurs de la construction</a></li>
+            <li><a href="<?php echo get_the_permalink(130); ?>">Bibliographie</a></li>
+            <li><a href="<?php echo get_the_permalink(131); ?>">Intervenants</a></li>
         </ul>
     </nav>
 </section>
--- template-parts/partials/home/home-about.php
+++ template-parts/partials/home/home-about.php
@@ -1,26 +1,32 @@
 <?php
 
 // get_fields
-$template = get_field('template','options');
+$template = get_field('template', 'options');
 $about = $template['about'];
 
 // contents
-$about_title = (!empty($about['title'])) ? '<h2>'.$about['title'].'</h2>' : '';
+$about_title = (!empty($about['title'])) ? '<p>' . $about['title'] . '</p>' : '';
 $about_content = (!empty($about['content'])) ? $about['content'] : '';
-$about_image = (!empty($about['image'])) ? '<img src="'.$about['image']['sizes']['theme_full'].'" alt="" >' : '';
+$about_image = (!empty($about['image'])) ? '<img src="' . $about['image']['sizes']['theme_full'] . '" alt="" >' : '';
 
 // buttons
-if(!empty($about['buttons'])):
+if (!empty($about['buttons'])) :
 	foreach ($about['buttons'] as $button) {
-		$button_echo .= '<a href="'.$button['link'].'" class="btn btn-link">'.$button['title'].'</a>';
+		$button_echo .= '<a href="' . $button['link'] . '" class="about__link link"><span>' . $button['title'] . '</span></a>';
 	}
 endif;
 
 ?>
 
-<div class="home-about">
-	<?php echo $about_title; ?>
-	<?php echo $about_content; ?>
-	<?php echo $about_image; ?>
-	<?php echo $button_echo; ?>
-</div>
+<article class="about">
+	<header> <?php echo $about_title; ?></header>
+	<section>
+		<div class="about__content">
+			<?php echo $about_content; ?>
+			<?php echo $button_echo; ?>
+		</div>
+		<figure>
+			<?php echo $about_image; ?>
+		</figure>
+	</section>
+</article>
