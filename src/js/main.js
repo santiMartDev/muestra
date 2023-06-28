0ceee7f1df5b568b89151445825fbabaae5391b6
Santiago Martinez <santiagomartinezlt@gmail.com>
2023-06-28 20:36:49 +0200

    Fetch Ok

--- src/js/main.js
+++ src/js/main.js
@@ -1,62 +1,43 @@
-// import menuHeader from "./modules/header.js";
-// import tabs from "./modules/tabs.js";
-// import lgGallery from "./modules/lightgallery.js";
-// import gallery from "./modules/gallery";
-// import artworks from "./modules/artworks";
-// import podcast from "./modules/podcast";
-// import map from "./modules/map.js";
+import menuHeader from "./modules/header.js";
+import tabs from "./modules/tabs.js";
+import lgGallery from "./modules/lightgallery.js";
+import gallery from "./modules/gallery";
+import artworks from "./modules/artworks";
+import podcast from "./modules/podcast";
+import map from "./modules/map.js";
 
-const instance = axios.create({
-  method: 'post',
-  // url: 'https://accounts.spotify.com/api/token',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded'
-  }, 
-  params: {
-    grant_type: 'client_credentials',
-    client_id: 'ecaf665a18ec4a92979a7ae31f4ccbb6',
-    client_secret: '9d744aa03a024388b02429b100e61bd5'
+podcast()
+
+
+const beforeStart = () => {
+  menuHeader();
+
+  const galleryExist = document.getElementsByClassName("gallery");
+  if (galleryExist.length > 0) {
+    // lgGallery();
+    gallery();
+
   }
+
+  const tabsExist = document.getElementsByClassName("tab");
+  if (tabsExist.length > 0) {
+    tabs();
+    artworks();
+  }
+};
+
+const start = () => {};
+
+// load
+window.addEventListener("load", () => {
+  beforeStart();
+
+  setTimeout(() => {
+    // const body = document.querySelector("body");
+    // body.classList.add("loaded");
+    start();
+
+  }, 1000);
 });
 
-
-async function request() {
-  const res = await instance.post('https://accounts.spotify.com/api/token');
-  console.log(res);
-}
-
-request();
-
-
-// const beforeStart = () => {
-//   menuHeader();
-
-//   const galleryExist = document.getElementsByClassName("gallery");
-//   if (galleryExist.length > 0) {
-//     // lgGallery();
-//     gallery();
-
-//   }
-
-//   const tabsExist = document.getElementsByClassName("tab");
-//   if (tabsExist.length > 0) {
-//     tabs();
-//     artworks();
-//   }
-// };
-
-// const start = () => {};
-
-// // load
-// window.addEventListener("load", () => {
-//   beforeStart();
-
-//   setTimeout(() => {
-//     // const body = document.querySelector("body");
-//     // body.classList.add("loaded");
-//     start();
-
-//   }, 1000);
-// });
-
-// map();
+map();
--- src/js/modules/podcast.js
+++ src/js/modules/podcast.js
@@ -1,15 +1,22 @@
-import { Player } from "shikwasa";
-
-const podcast = () => {
-  const player = new Player({
-    container: () => document.querySelector(".podcast"),
-    audio: {
-      title: "Hello World!",
-      artist: "Shikwasa FM",
-      cover: "image.png",
-      src: "https://open.spotify.com/track/5hFkGfx038V0LhqI0Uff2J?si=b7256a3e481d4a4a",
-    },
-  });
+const params = {
+  client_id: "ecaf665a18ec4a92979a7ae31f4ccbb6",
+  client_secret: "9d744aa03a024388b02429b100e61bd5",
 };
+
+const options = {
+  method: "POST",
+  body: `grant_type=client_credentials&client_id=${params.client_id}&client_secret=${params.client_secret}`,
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+  },
+  mode: "cors",
+  cache: "default",
+};
+
+async function podcast() {
+  const res = await fetch("https://accounts.spotify.com/api/token", options);
+  const data = await res.json();
+  console.log(data);
+}
 
 export default podcast;
