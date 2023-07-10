cbb1767c2cbe62d26f2f4582562191c05d093f1c
Santiago Martinez <santiagomartinezlt@gmail.com>
2023-07-10 16:20:08 +0200

    Debug

--- single-theme.php
+++ single-theme.php
@@ -2,89 +2,93 @@
 
 <?php
 
-if ( have_posts() ) : while ( have_posts() ) : the_post();
+if (have_posts()) : while (have_posts()) : the_post();
 
-	// title
-	$page_title = get_the_title();
-	require( TEMPLATEPATH . '/template-parts/partials/page-header.php' );
+        // title
+        $page_title = get_the_title();
+        require(TEMPLATEPATH . '/template-parts/partials/page-header.php');
 
-    // argumentos query themes
-    $args = array(
-        'post_type'       	=> 'theme',
-        'posts_per_page'  	=> 1,
-        'p'                 => get_the_ID()
-    );
-
-    // get all themes
-    $themes = new WP_Query( $args );
-
-    // get terms taxonomy of this theme
-    $terms = get_the_terms( get_the_ID(), 'project_theme' );
-
-    // loop terms taxonomy
-    foreach ($terms as $term) {
-
-        // argumentos query - projects by term taxonomy
-        $args_education = array(
-            'post_type'       	=> 'education',
-            'posts_per_page'  	=> -1,
-            'orderby' 			=> 'menu-order',
-            'order'				=> 'ASC',
-            'tax_query' => array(
-                array(
-                    'taxonomy' => 'project_theme',
-                    'field' => 'slug',
-                    'terms' => $term->name
-                ),
-            ),
+        // argumentos query themes
+        $args = array(
+            'post_type'           => 'theme',
+            'posts_per_page'      => 1,
+            'p'                 => get_the_ID()
         );
 
-        // get all projects by term taxonomy
-        $educations = new WP_Query( $args_education );
+        // get all themes
+        $themes = new WP_Query($args);
 
-        // argumentos query - projects by term taxonomy
-        $args_project = array(
-            'post_type'       	=> 'project',
-            'posts_per_page'  	=> -1,
-            'orderby' 			=> 'menu-order',
-            'order'				=> 'ASC',
-            'tax_query' => array(
-                array(
-                    'taxonomy' => 'project_theme',
-                    'field' => 'slug',
-                    'terms' => $term->name
+        // get terms taxonomy of this theme
+        $terms = get_the_terms(get_the_ID(), 'project_theme');
+
+        // loop terms taxonomy
+        foreach ($terms as $term) {
+
+            // argumentos query - projects by term taxonomy
+            $args_education = array(
+                'post_type'           => 'education',
+                'posts_per_page'      => -1,
+                'orderby'             => 'menu-order',
+                'order'                => 'ASC',
+                'tax_query' => array(
+                    array(
+                        'taxonomy' => 'project_theme',
+                        'field' => 'slug',
+                        'terms' => $term->name
+                    ),
                 ),
-            ),
-        );
+            );
 
-        // get all projects by term taxonomy
-        $projects = new WP_Query( $args_project );
+            // get all projects by term taxonomy
+            $educations = new WP_Query($args_education);
 
-        $sidebar_theme_projects[] = array('title' => __('Related projects','imaneo'), 'link' => '', 'blank' => false);
+            // argumentos query - projects by term taxonomy
+            $args_project = array(
+                'post_type'           => 'project',
+                'posts_per_page'      => -1,
+                'orderby'             => 'menu-order',
+                'order'                => 'ASC',
+                'tax_query' => array(
+                    array(
+                        'taxonomy' => 'project_theme',
+                        'field' => 'slug',
+                        'terms' => $term->name
+                    ),
+                ),
+            );
 
-        // loop projects by term taxonomy
-        if ( $projects->have_posts() ) {
-            while ( $projects->have_posts() ) {  $projects->the_post();
-                $sidebar_theme_projects[] = array('title' => get_the_title(),  'link' => array('url' => get_the_permalink()), 'blank' => false);
-            } wp_reset_postdata();
+            // get all projects by term taxonomy
+            $projects = new WP_Query($args_project);
+
+            $sidebar_theme_projects[] = array('title' => __('Related projects', 'imaneo'), 'link' => '', 'blank' => false);
+
+            // loop projects by term taxonomy
+            if ($projects->have_posts()) {
+                while ($projects->have_posts()) {
+                    $projects->the_post();
+                    $sidebar_theme_projects[] = array('title' => get_the_title(),  'link' => array('url' => get_the_permalink()), 'blank' => false);
+                }
+                wp_reset_postdata();
+            }
+
+            // loop projects by term taxonomy
+            if ($educations->have_posts()) {
+                while ($educations->have_posts()) {
+                    $educations->the_post();
+                    $sidebar_theme_education[] = array('title' => get_the_title(), 'link' => array('url' => get_the_permalink()), 'blank' => false);
+                }
+                wp_reset_postdata();
+            }
+
+            if ($educations->have_posts() || $projects->have_posts()) {
+                $sidebar_menus = array($sidebar_theme_education, $sidebar_theme_projects);
+            }
         }
 
-        // loop projects by term taxonomy
-        if ( $educations->have_posts() ) {
-            while ( $educations->have_posts() ) {  $educations->the_post();
-                $sidebar_theme_education[] = array('title' => get_the_title(), 'link' => array('url' => get_the_permalink()), 'blank' => false);
-            } wp_reset_postdata();
-        }
+        require(TEMPLATEPATH . '/template-parts/modules/media.php');
 
-        if($educations->have_posts() || $projects->have_posts() ){
-            $sidebar_menus = array( $sidebar_theme_education, $sidebar_theme_projects );
-        }
-
-    }
-
-    require( TEMPLATEPATH . '/template-parts/modules/theme.php' );
-
-endwhile; endif;
+    endwhile;
+endif;
 
 ?>
 
--- template-parts/modules/media.php
+++ template-parts/modules/media.php
@@ -1,44 +1,47 @@
-<?php if (!empty($media['video'] )) : ?>
+<?php if (!empty($media['video'])) : ?>
 
-    <article class="video">
-        <iframe width="1280" height="720" src="<?php echo $media['video']; ?>" allowfullscreen></iframe>
-    </article>
+<article class="video">
+    <iframe src="https://player.vimeo.com/video/<?php echo $media['video']; ?>" frameborder="0" webkitallowfullscreen
+        mozallowfullscreen allowfullscreen></iframe>
+</article>
 
 <?php elseif (!empty($media['gallery'])) : ?>
 
-    <div class="gallery-wrap">
+<div class="gallery-wrap">
 
-        <article class="gallery splide">
-            <div class="splide__arrows splide__arrows--ltr">
-                <button class="splide__arrow splide__arrow--prev" type="button" aria-label="Previous slide" aria-controls="splide01-track">
-                    <img src="<?php bloginfo('template_url') ?>/dist/assets/icons/icon-arrow-prev.svg" alt="">
-                </button>
-                <button class="splide__arrow splide__arrow--next" type="button" aria-label="Next slide" aria-controls="splide01-track">
-                    <img src="<?php bloginfo('template_url') ?>/dist/assets/icons/icon-arrow-next.svg" alt="">
-                </button>
-            </div>
-            <div class="splide__track">
-                <div class="splide__list">
-                    <?php foreach ($media['gallery'] as $img) { ?>
-                        <div class="splide__slide">
-                            <img src="<?php echo $img['sizes']['theme_full']; ?>" alt="">
-                        </div>
-                    <?php } ?>
+    <article class="gallery splide">
+        <div class="splide__arrows splide__arrows--ltr">
+            <button class="splide__arrow splide__arrow--prev" type="button" aria-label="Previous slide"
+                aria-controls="splide01-track">
+                <img src="<?php bloginfo('template_url') ?>/dist/assets/icons/icon-arrow-prev.svg" alt="">
+            </button>
+            <button class="splide__arrow splide__arrow--next" type="button" aria-label="Next slide"
+                aria-controls="splide01-track">
+                <img src="<?php bloginfo('template_url') ?>/dist/assets/icons/icon-arrow-next.svg" alt="">
+            </button>
+        </div>
+        <div class="splide__track">
+            <div class="splide__list">
+                <?php foreach ($media['gallery'] as $img) { ?>
+                <div class="splide__slide">
+                    <img src="<?php echo $img['sizes']['theme_full']; ?>" alt="">
                 </div>
+                <?php } ?>
             </div>
-        </article>
-        <article class="splide thumbs">
-            <div class="splide__track">
-                <div class="splide__list">
-                    <?php foreach ($media['gallery'] as $img) { ?>
-                        <div class="splide__slide">
-                            <img src="<?php echo $img['sizes']['theme_full']; ?>" alt="">
-                        </div>
-                    <?php } ?>
+        </div>
+    </article>
+    <article class="splide thumbs">
+        <div class="splide__track">
+            <div class="splide__list">
+                <?php foreach ($media['gallery'] as $img) { ?>
+                <div class="splide__slide">
+                    <img src="<?php echo $img['sizes']['theme_full']; ?>" alt="">
                 </div>
+                <?php } ?>
             </div>
-        </article>
+        </div>
+    </article>
 
-    </div>
+</div>
 
 <?php endif; ?>
