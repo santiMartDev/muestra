1d281246dd3efedd72001498d4df8357515a047d
degranofino <58522315+degranofino@users.noreply.github.com>
2023-07-10 20:18:48 +0200

    sidebar debug

--- template-parts/project/project-history.php
+++ template-parts/project/project-history.php
@@ -5,21 +5,24 @@
 // assign history field to module artist
 $history = $project['history'];
 
-// artist sidebar and all button
-$sidebar = (!empty($history['sidebar'])) ? $history['sidebar'] : '';
-
-$sidebar_artist_all = array(
+// sidebar default
+$sidebar_history = array(
     array('title' => __('Construction actors', 'imaneo'), 'link' => array('url' => get_the_permalink(129)), 'blank' => false),
     array('title' => __('Bibliography', 'imaneo'), 'link' => array('url' => get_the_permalink(130)), 'blank' => false),
     array('title' => __('Educational activities', 'imaneo'), 'link' => array('url' => get_the_permalink(142)), 'blank' => false),
 );
 
-$sidebar = array_merge($sidebar_artist_all, $sidebar);
+// sidebar project history
+if(!empty($history['sidebar'])) {
+    $sidebar = array_merge($sidebar_history, $history['sidebar'] );
+} else {
+    $sidebar = array_merge($sidebar_history);
+}
 
-// config menus from artist
+// config menus from project
 $sidebar_menus = array($sidebar);
 
-// include module education
+// include module history
 require( TEMPLATEPATH . '/template-parts/modules/history.php' );
 
 ?>
--- template-parts/project/project-podcast.php
+++ template-parts/project/project-podcast.php
@@ -6,9 +6,15 @@
 $podcast = $project['podcast'];
 
 // artist sidebar and all button
-$sidebar = (!empty($podcast['sidebar'])) ? $podcast['sidebar'] : '';
 $sidebar_podcast_all = array( array('title' => __('All the words of people', 'imaneo'), 'link' => array('url' => get_the_permalink(132)), 'blank' => false) );
 $sidebar = array_merge($sidebar_podcast_all, $sidebar);
+
+// sidebar project podcast
+if(!empty($podcast['sidebar'])) {
+    $sidebar = array_merge($sidebar_podcast_all, $podcast['sidebar'] );
+} else {
+    $sidebar = array_merge($sidebar_podcast_all);
+}
 
 // config menus from podcast
 $sidebar_menus = array($sidebar);
