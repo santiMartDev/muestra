12bb3d3a9c1662fa781a5eb6fb090e50ba772bc7
degranofino <58522315+degranofino@users.noreply.github.com>
2023-07-11 12:53:00 +0200

    history sidebar debug

--- template-parts/project/project-history.php
+++ template-parts/project/project-history.php
@@ -3,24 +3,29 @@
 <?php
 
 // assign history field to module artist
-$history = $project['history'];
+// $history = $project['history'];
 
-// sidebar default
-$sidebar_history = array(
-    array('title' => __('Construction actors', 'imaneo'), 'link' => array('url' => get_the_permalink(129)), 'blank' => false),
-    array('title' => __('Bibliography', 'imaneo'), 'link' => array('url' => get_the_permalink(130)), 'blank' => false),
-    array('title' => __('Educational activities', 'imaneo'), 'link' => array('url' => get_the_permalink(142)), 'blank' => false),
-);
+// // sidebar default
+// $sidebar_history = array(
+//     array('title' => __('Construction actors', 'imaneo'), 'link' => array('url' => get_the_permalink(129)), 'blank' => false),
+//     array('title' => __('Bibliography', 'imaneo'), 'link' => array('url' => get_the_permalink(130)), 'blank' => false),
+//     array('title' => __('Educational activities', 'imaneo'), 'link' => array('url' => get_the_permalink(142)), 'blank' => false),
+// );
+
+// // sidebar project history
+// if(!empty($history['sidebar'])) {
+//     $sidebar = array_merge($sidebar_history, $history['sidebar'] );
+// } else {
+//     $sidebar = array_merge($sidebar_history);
+// }
+
+// // config menus from project
+// $sidebar_menus = array($sidebar);
 
 // sidebar project history
 if(!empty($history['sidebar'])) {
-    $sidebar = array_merge($sidebar_history, $history['sidebar'] );
-} else {
-    $sidebar = array_merge($sidebar_history);
+    $sidebar_menus = array($history['sidebar']);
 }
-
-// config menus from project
-$sidebar_menus = array($sidebar);
 
 // include module history
 require( TEMPLATEPATH . '/template-parts/modules/history.php' );
