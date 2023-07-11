6942f5aa1ce1c1d9ddf021fae8f0c9a2317fc30e
chisco <chisco@degranofino.com>
2023-07-11 03:00:06 +0200

    artistas debug

--- template-parts/partials/artworks/artworks-list.php
+++ template-parts/partials/artworks/artworks-list.php
@@ -17,41 +17,39 @@ $projects = new WP_Query($args);
 
 <div class="artworks-list">
 
-    <?php while ($projects->have_posts()) {
-            $projects->the_post();
+    <?php while ($projects->have_posts()) { $projects->the_post();
 
-            $project = get_field('projects');
+        $project = get_field('projects');
 
-            // var_dump($project);
+        // var_dump($project);
 
-            // assign artist field to module artist
-            $artist = $project['artist'];
-            $info = $project['project_info'];
+        // assign artist field to module artist
+        $artist = $project['artist'];
+        $info = $project['project_info'];
 
-            $sidebar = array(
-                array('title' => __('Related Project', 'imaneo'), 'link' => '', 'blank' => false),
-                array('title' => get_the_title(), 'link' => array('url' => get_the_permalink()), 'blank' => false),
-            );
+        $sidebar = array(
+            array('title' => __('Related Project', 'imaneo'), 'link' => '', 'blank' => false),
+            array('title' => get_the_title(), 'link' => array('url' => get_the_permalink()), 'blank' => false),
+        );
 
-            $sidebar_menus = array($sidebar);
-            $media = (!empty($artist['media'])) ? $artist['media'] : '';
+        $sidebar_menus = array($sidebar);
+        $media = (!empty($artist['media'])) ? $artist['media'] : '';
 
-            if ($media) : ?>
+        if ( $media['gallery'] || !empty($media['video'])) : ?>
 
-    <div class="artworks-item">
+            <div class="artworks-item">
 
-        <div class="lists-title">
-            <img src="<?php echo $info['icon']['url']; ?>" alt="">
-            <p><?php echo get_the_title(); ?></p>
-        </div>
-        <?php require(TEMPLATEPATH . '/template-parts/modules/artworks.php'); ?>
+                <div class="lists-title">
+                    <img src="<?php echo $info['icon']['url']; ?>" alt="">
+                    <p><?php echo get_the_title(); ?></p>
+                </div>
+                <?php require(TEMPLATEPATH . '/template-parts/modules/artworks.php'); ?>
 
-    </div>
+            </div>
 
-    <?php endif; ?>
+        <?php endif; ?>
 
-    <?php }
-        wp_reset_postdata();  ?>
+    <?php } wp_reset_postdata();  ?>
 
 </div>
 
--- template-parts/partials/podcast/podcast-list.php
+++ template-parts/partials/podcast/podcast-list.php
@@ -17,40 +17,37 @@ $projects = new WP_Query($args);
 
 <div class="podcast-list">
 
-    <?php while ($projects->have_posts()) {
-            $projects->the_post();
+    <?php while ($projects->have_posts()) { $projects->the_post();
 
-            $project = get_field('projects');
+        $project = get_field('projects');
 
+        // assign artist field to module artist
+        $podcast = $project['podcast'];
+        $info = $project['project_info'];
 
-            // assign artist field to module artist
-            $podcast = $project['podcast'];
-            $info = $project['project_info'];
+        $sidebar = array(
+            array('title' => __('Related Project', 'imaneo'), 'link' => '', 'blank' => false),
+            array('title' => get_the_title(), 'link' => array('url' => get_the_permalink()), 'blank' => false),
+        );
 
-            $sidebar = array(
-                array('title' => __('Related Project', 'imaneo'), 'link' => '', 'blank' => false),
-                array('title' => get_the_title(), 'link' => array('url' => get_the_permalink()), 'blank' => false),
-            );
+        $sidebar_menus = array($sidebar);
+        $podcasts = (!empty($podcast['podcast'])) ? $podcast['podcast'] : '';
 
-            $sidebar_menus = array($sidebar);
-            $podcasts = (!empty($podcast['podcast'])) ? $podcast['podcast'] : '';
+        if ($podcasts) : ?>
 
-            if ($podcasts) : ?>
+            <div class="podcast-item">
 
-    <div class="podcast-item">
+                <div class="lists-title">
+                    <img src="<?php echo $info['icon']['url']; ?>" alt="">
+                    <p><?php echo get_the_title(); ?></p>
+                </div>
+                <?php require(TEMPLATEPATH . '/template-parts/modules/podcast.php'); ?>
 
-        <div class="lists-title">
-            <img src="<?php echo $info['icon']['url']; ?>" alt="">
-            <p><?php echo get_the_title(); ?></p>
-        </div>
-        <?php require(TEMPLATEPATH . '/template-parts/modules/podcast.php'); ?>
+            </div>
 
-    </div>
+        <?php endif; ?>
 
-    <?php endif; ?>
-
-    <?php }
-        wp_reset_postdata();  ?>
+    <?php } wp_reset_postdata();  ?>
 
 </div>
 
