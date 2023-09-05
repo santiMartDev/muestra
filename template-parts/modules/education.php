85989b0150423b9def9c2676d7339ae5543114e6
Santiago Martinez <santiagomartinezlt@gmail.com>
2023-09-05 16:46:52 +0200

    Edu fixed

--- template-parts/modules/education.php
+++ template-parts/modules/education.php
@@ -9,7 +9,7 @@ $title = $content_post->post_title;
 
 $motif = get_field('education', $education_id);
 
-var_dump($motif['img']);
+// var_dump($motif['download']);
 ?>
 
 <p class="title__edu"><?php echo $title;  ?></p>
@@ -20,7 +20,7 @@ var_dump($motif['img']);
 <section class="module-default">
 
     <section>
-        <a href="<?php echo $motif['download'] ?>">
+        <a href="<?php echo $motif['download']['url'] ?>" target="_blank">
             <img src="<?php echo $motif['img'] ?>" alt="">
         </a>
     </section>
