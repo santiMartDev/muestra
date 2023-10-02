5589668318bc3be43176c4525dd0cdffee1977ac
chisco <chisco@degranofino.com>
2023-10-02 10:37:31 +0200

    language switcher ddebug

--- inc/helper/language-switcher.php
+++ inc/helper/language-switcher.php
@@ -15,16 +15,13 @@ if (!function_exists('ffTheme_language_switcher')) :
 
 		$languages = icl_get_languages('skip_missing=1');
 
-		if (1 < count($languages)) {
-			foreach ($languages as $code => $l) {
-				if (!$l['active']) $langs2[] = '<li class="inactive"><a href="' . $l['url'] . '">' . strtoupper($code) . '</a></li>';
-				if ($l['active']) $langs1[] = '<li class="active"><span>' . strtoupper($code) . '</span></li>';
+
+		if(1 < count($languages)){
+			foreach($languages as $code => $l){
+			  if(!$l['active']) $langs[] = '<li class="inactive"><a href="'.$l['url'].'">'.strtoupper($code).'</a></li>';
+			  if($l['active']) $langs[] = '<li class="active"><span>'.strtoupper($code).'</span></li>';
 			}
-
-			$lang_ext[] = $langs1[0];
-			$lang_ext[] = $langs2[0];
-
-			echo join($lang_ext);
+			echo join($langs);
 		}
 	}
 endif;
